b60f38e28b11e66a4a4ff3362b8a6ee62b62f18d
Peter Nylander <peter.nylander@gmail.com>
2024-12-30 12:35:58 +0100

    Add custom exception handler and problem details support
    Introduce `ExceptionToProblemDetailsHandler` to handle exceptions and create detailed problem responses. Update `Program.cs` to register the new handler and customize problem details with trace ID and request method. Add middleware for status code handling and configure HTTPS redirection.

---
+++ src/Odin.Api/ExceptionHandlers/ExceptionToProblemDetailsHandler.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Odin.Api.ExceptionHandlers;
+
+public class ExceptionToProblemDetailsHandler(IProblemDetailsService problemDetailsService) : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        return await problemDetailsService.TryWriteAsync(new()
+        {
+            Exception = exception,
+            HttpContext = httpContext,
+            ProblemDetails = new ProblemDetails
+            {
+                Title = "An error occurred",
+                Detail = exception.Message,
+                Type = exception.GetType().Name,
+            }
+        });
+    }
+}
--- src/Odin.Api/Program.cs
+++ src/Odin.Api/Program.cs
@@ -1,3 +1,5 @@
+using Odin.Api.ExceptionHandlers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
@@ -9,7 +11,25 @@ builder.Services.AddHttpContextAccessor();
 builder.Services.AddAuthentication().AddJwtBearer();
 builder.Services.AddAuthorization();
 
+builder.Services.AddProblemDetails(options =>
+    options.CustomizeProblemDetails = ctx =>
+    {
+        ctx.ProblemDetails.Extensions["trace-id"] = ctx.HttpContext.TraceIdentifier;
+        ctx.ProblemDetails.Extensions["instance"] = ctx.HttpContext.Request.Method;
+    });
+builder.Services.AddExceptionHandler<ExceptionToProblemDetailsHandler>();
+
 var app = builder.Build();
+
+app.UseStatusCodePages();
+app.UseExceptionHandler(new ExceptionHandlerOptions
+{
+    StatusCodeSelector = ex => ex switch
+    {
+        InvalidOperationException _ => StatusCodes.Status400BadRequest,
+        _ => StatusCodes.Status500InternalServerError,
+    },
+});
 
 app.MapDefaultEndpoints();
 
