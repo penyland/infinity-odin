6787a428db10874c88a50f0a94f9bfa611a668aa
Peter Nylander <peter.nylander@gmail.com>
2025-01-18 13:52:57 +0100

    Add scalar command functionality to resource builder
    Introduces `OdinApiResourceBuilderExtensions` with the
    `WithScalarCommand<T>` method to enable scalar command
    execution that opens a URL in the default browser.
    Includes private methods for command execution and state
    management based on resource health status. Updates
    `Program.cs` to utilize the new command functionality
    in the API project.

---
+++ src/Odin.AppHost/OdinApiResourceBuilderExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Diagnostics;
+
+namespace Odin.AppHost;
+
+internal static class OdinApiResourceBuilderExtensions
+{
+    public static IResourceBuilder<T> WithScalarCommand<T>(this IResourceBuilder<T> builder)
+        where T : IResourceWithEndpoints
+    {
+        builder.WithCommand(
+            name: "scalar-docs",
+            displayName: "Scalar api reference",
+            executeCommand: context => OnOpenUrlCommandAsync(builder, context, "scalar"),
+            updateState: OnUpdateStateResource,
+            iconName: "AnimalRabbitOff",
+            iconVariant: IconVariant.Filled);
+
+        return builder;
+    }
+
+    private static async Task<ExecuteCommandResult> OnOpenUrlCommandAsync<T>(IResourceBuilder<T> builder, ExecuteCommandContext context, string url)
+        where T : IResourceWithEndpoints
+    {
+        var endpoint = builder.Resource.GetEndpoint("https").Url;
+        var theUrl = $"{endpoint}/{url}";
+        Console.WriteLine(theUrl);
+        await Task.Run(() => Process.Start(new ProcessStartInfo
+        {
+            UseShellExecute = true,
+            FileName = theUrl,
+        }));
+        return CommandResults.Success();
+    }
+
+    private static ResourceCommandState OnUpdateStateResource(UpdateCommandStateContext context)
+    {
+        return context.ResourceSnapshot.HealthStatus == HealthStatus.Healthy
+                    ? ResourceCommandState.Enabled
+                    : ResourceCommandState.Disabled;
+    }
+}
